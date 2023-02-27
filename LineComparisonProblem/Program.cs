360a8ad00b747749c22c8d275c7203526815569c
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 18:30:14 +0530

    [Prabhu] Add: Compare Two Lines

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -4,10 +4,12 @@ public class Program
 {
     static void Main(string[] args)
     {
+        //UC1
         Console.WriteLine("Welcome To Line Comparision Problem Statement");
         LineComparison lineComparison = new LineComparison(8, 6, 4, 2);
         lineComparison.CalculateLength();
 
+        //UC2
         LineComparison calculateLength1 = new LineComparison(1, 2, 3, 4);
         double lenght1 = calculateLength1.Calculate();
         LineComparison calculateLength2 = new LineComparison(1, 3, 3, 4);
@@ -17,5 +19,22 @@ public class Program
             Console.WriteLine("Both lines are equal");
         else
             Console.WriteLine("Both Lines are not equal");
+        Console.WriteLine("\n");
+
+        //UC3
+        LineComparison lineCompare3 = new LineComparison(1, 4, 1, 4);
+        double lenght3 = lineCompare3.Calculate();
+        LineComparison lineCompare4 = new LineComparison(1, 3, 2, 4);
+        double lenght4 = lineCompare4.Calculate();
+
+        if (lenght3.CompareTo(lenght4) == 0)
+        {
+            Console.WriteLine("Line 1 and Line 2 both are equal");
+            return;
+        }
+        if (lenght3.CompareTo(lenght4) > 0)
+            Console.WriteLine("Line 1 is greate than Line 2");
+        else
+            Console.WriteLine("Line 2 is greater than Line 1");
     }
 }
